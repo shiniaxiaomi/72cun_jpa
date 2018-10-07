e8ae9ab7f552a5b6bcb2cc8fd841b70e2dfa7722
陆英杰 <34340797+shiniaxiaomi@users.noreply.github.com>
2018-10-07 19:54:16 +0800

    删除静态资源

---
+++ src/main/java/com/lyj/controller/PageController.java
@@ -0,0 +1,26 @@
+package com.lyj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Yingjie.Lu on 2018/10/7.
+ */
+
+@Controller
+public class PageController {
+
+
+    @RequestMapping("/")
+    public String toLogin(){
+        return "login";
+    }
+
+
+    @RequestMapping("/111")
+    public String aaa(){
+        return "111";
+    }
+
+}
